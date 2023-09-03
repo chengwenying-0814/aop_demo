5605e01a676de888f39398c343389a327182b442
chengwenying <123456>
2023-09-03 22:27:53 +0800

    第一次提交

---
+++ src/main/java/com/example/study_demo/StudyDemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.study_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cheng
+ */
+@SpringBootApplication
+public class StudyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyDemoApplication.class, args);
+
+    }
+
+}
---
+++ src/main/java/com/example/study_demo/controller/LogbackController.java
@@ -0,0 +1,41 @@
+package com.example.study_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName: LogbackController
+ * PackageName: com.example.study_demo
+ * Describe:
+ *
+ * @author: 成文莹
+ * @Create: 2023/9/3 - 20:45
+ * @version:
+ */
+@RestController
+@RequestMapping("/log")
+public class LogbackController {
+    /**
+     * 测试正常请求
+     * @param msg
+     * @return
+     */
+    @GetMapping("/normal/{msg}")
+    public String getMsg(@PathVariable String msg) {
+        return msg;
+    }
+
+    /**
+     * 测试抛异常
+     * @return
+     */
+    @GetMapping("/exception/{msg}")
+    public String getException(@PathVariable String msg){
+        // 故意造出一个异常
+        Integer.parseInt("abc123");
+        return msg;
+    }
+}
